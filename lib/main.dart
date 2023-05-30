97e699b7742c55c12fdbe5b0d7e1b4398e455248
KP Shao <shaokangpeng@semitronix.com>
2023-05-30 11:34:34 +0800

    complete project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,59 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text(
+          'Ask Me Anything',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: false,
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Container(
+        child: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int _ballNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            _ballNumber = Random().nextInt(5) + 1;
+          });
+          print('$_ballNumber');
+        },
+        child: Image.asset('images/ball$_ballNumber.png'),
+      ),
+    );
+  }
+}
